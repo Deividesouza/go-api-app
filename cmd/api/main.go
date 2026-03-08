8a4cf4f18dc7a260e20403880d092d5fbf9b0200
Deividesouza <deivide111@gmail.com>
2026-03-08 15:52:10 -0300

    fix: corrige o nome do pacote para 'main' no arquivo main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,4 @@
-package goapiapp
+package main
 
 import (
 	"fmt"
@@ -9,4 +9,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
+	http.ListenAndServe(":8080", nil)
 }
