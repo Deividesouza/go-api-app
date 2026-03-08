c4735f9953dae253224645549b11e1c5f1221e2f
Deividesouza <deivide111@gmail.com>
2026-03-08 13:40:35 -0300

    adiciona arquivos iniciais: .gitignore, go.mod e main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Deividesouza/go-api-app
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package goapiapp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+}
